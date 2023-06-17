0a0a2609c123e48dd0e8d55456d74769e5d83874
islom <islomjon20010930@gmail.com>
2023-06-18 03:17:40 +0500

    dastlabki qadamlar asset, font, svg, papkalar, fayllar, utils joylashtirib chiqildi

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const Color c_000000 = Color(0xFF000000);
+  static const Color c_FFFFFF = Color(0xFFFFFFFF);
+  static const Color c_FF0000 = Color(0xFFFF0000);
+}
---
+++ lib/utils/icons.dart
@@ -0,0 +1,21 @@
+class IconsApp {
+  // svg
+  static const String back = 'assets/svg/back.svg';
+  static const String bookmark = 'assets/svg/bookmark.svg';
+  static const String bookmarkBlack = 'assets/svg/bookmark_black.svg';
+  static const String category = 'assets/svg/category.svg';
+  static const String chat = 'assets/svg/chat.svg';
+  static const String dot = 'assets/svg/dot.svg';
+  static const String edit = 'assets/svg/edit.svg';
+  static const String exit = 'assets/svg/exit.svg';
+  static const String home = 'assets/svg/home.svg';
+  static const String language = 'assets/svg/language.svg';
+  static const String profile = 'assets/svg/profile.svg';
+  static const String search = 'assets/svg/search.svg';
+  static const String send = 'assets/svg/send.svg';
+  static const String user = 'assets/svg/user.svg';
+
+  // jpg
+  static const String background =
+      "assets/images/green-leafed-plant-iphone-13-pro-max.jpg";
+}
